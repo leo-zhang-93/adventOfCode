c55a606402df5eae0cc7e3753322ed145017676e
Hanchi Zhang <leo.hc.zhang@gmail.com>
2024-12-01 20:09:25 +0800

    Changed the scripts to using more standard testing.

--- 2024/script_20241201.py
+++ 2024/script_20241201.py
@@ -1,14 +1,4 @@
-"""
-This script reads a text file containing pairs of integers, parses the data,
-sorts the integers, and performs calculations on the sorted lists.
-
-Args:
-    path (str): The path to the input file.
-
-Returns:
-    tuple: A tuple containing two sorted lists of integers.
-"""
-def calc1(path):
+def getList(path):
     """Reads and parses the input file, returning sorted lists.
 
     Args:
@@ -18,8 +8,8 @@ def calc1(path):
         tuple: A tuple containing two sorted lists of integers.
     """
 
-    ls1: list[Any] = []
-    ls2: list[Any] = []
+    ls1 = []
+    ls2 = []
 
     try:
         with open(path, 'r') as f:
@@ -37,26 +27,29 @@ def calc1(path):
     return ls1, ls2
 
 
-if __name__ == "__main__":
-    # path1 = 'inputPartial_20241201.txt'
-    path1 = 'inputFull_20241201.txt'
-    ls1, ls2 = calc1(path1)
+def calc1(path):
+    ls1, ls2 = getList(path)
     ls1 = sorted(ls1)
     ls2 = sorted(ls2)
-
-    # ... further processing of ls1 and ls2 ...
-    # For example, the calculations from your original code:
-
-    # Part 1:
     n = len(ls1)
     print(sum([abs(ls1[i] - ls2[i]) for i in range(n)]))
 
-    # Part 2:
-    from collections import Counter
 
+def calc2(path):
+    from collections import Counter
+    ls1, ls2 = getList(path)
     res = 0
     counter1 = Counter(ls1)
     counter2 = Counter(ls2)
     for key in counter1.keys():
         res += key * counter1[key] * counter2[key]
     print(res)
+
+
+if __name__ == "__main__":
+    # path1 = 'inputPartial_20241201.txt'
+    path1 = 'inputFull_20241201.txt'
+    # Part 1:
+    calc1(path1)
+    # Part 2:
+    calc2(path1)
