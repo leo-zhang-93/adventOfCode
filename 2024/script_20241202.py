31815246a7624b1395027ce842576cbb8dec530a
Hanchi Zhang <leo.hc.zhang@gmail.com>
2024-12-02 13:23:35 +0800

    Cleaned one file and added a new file.

---
+++ 2024/script_20241202.py
@@ -0,0 +1,61 @@
+def getList(path):
+    """Reads and parses the input file, returning sorted lists.
+
+    Args:
+        path (str): The path to the input file.
+
+    Returns:
+        tuple: A tuple containing two sorted lists of integers.
+    """
+    ls = []
+
+    try:
+        with open(path, 'r') as f:
+            for line in f:
+                ls.append([item for item in map(int, line[:-1].split(' '))])
+    except FileNotFoundError:
+        print(f"File not found: {path}")
+        return []
+    except ValueError:
+        print(f"Invalid input format in {path}")
+        return []
+
+    return ls
+
+
+def check1(ls):
+    increase = True
+    if ls[1] < ls[0]:
+        increase = False
+    n = len(ls)
+    for i in range(1, n):
+        diff = ls[i] - ls[i - 1]
+        if (increase and 1 <= diff <= 3) or (not increase and -3 <= diff <= -1):
+            continue
+        else:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    path1 = 'inputPartial_20241202.txt'
+    # path1 = 'inputFull_20241202.txt'
+    ls = getList(path1)
+
+    # Part 1
+    res1 = 0
+    for item in ls:
+        if check1(item):
+            res1 += 1
+    print(res1)
+
+    # Part 2
+    res2 = 0
+    for item in ls:
+        lenitem = len(item)
+        for j in range(lenitem):
+            if check1(item[:j] + item[j + 1:]):
+                # print(item, item[:lenitem] + item[lenitem + 1:])
+                res2 += 1
+                break
+    print(res2)
